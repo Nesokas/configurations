3a15b2c67511fb78b21a126a10dc42763a3cb77e
Nesokas <ines.moital@gmail.com>
2013-05-02 15:19:54 +0100

    even better configuration

--- config.sh
+++ config.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 config_path=`pwd`
 cd ~
-rm -rf .vim/ .vimrc .profile
+rm -rf .vim .vimrc .profile
 ln -s $config_path/.vim .vim
 ln -s $config_path/.vimrc .vimrc
 ln -s $config_path/.profile .profile
+rm -rf ~/.vim/bundle/vundle
 git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
 vim << EOF
 :BundleInstall
